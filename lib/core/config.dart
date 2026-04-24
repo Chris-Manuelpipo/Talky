1998333de50dd0309f1cc176012ea1ccbb0ea1e9
Chris Manuel <etchomechris2000@gmail.com>
2026-04-24 20:46:52 +0100

    fix: use http instead of https in API URLs

--- lib/core/config.dart
+++ lib/core/config.dart
@@ -1,36 +1,36 @@
 // lib/core/config.dart
- 
+
 class AppConfig {
   AppConfig._();
 
   //static const String _kDefaultHost = 'talky-signaling.onrender.com';
-  static const String _kDefaultHost = 'http://158.220.107.211';
+  static const String _kDefaultHost = '158.220.107.211';
 
   static String get signalingUrl {
     return String.fromEnvironment(
       'SIGNALING_URL',
-      defaultValue: 'https://$_kDefaultHost',
+      defaultValue: 'http://$_kDefaultHost/api',
     );
   }
 
   static String get apiUrl {
     return String.fromEnvironment(
       'API_URL',
-      defaultValue: 'https://$_kDefaultHost/api',
+      defaultValue: 'http://$_kDefaultHost/api',
     );
   }
 
   static String get socketUrl {
     return String.fromEnvironment(
       'SOCKET_URL',
-      defaultValue: 'https://$_kDefaultHost',
+      defaultValue: 'http://$_kDefaultHost/api',
     );
   }
 
   static String get notifyUrl {
     return String.fromEnvironment(
       'NOTIFY_URL',
-      defaultValue: 'https://$_kDefaultHost/api/notify',
+      defaultValue: 'http://$_kDefaultHost/api/notify',
     );
   }
 }
